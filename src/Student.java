a7fb3b78b010daf5cff0a1cdd88bcdb5bd753ab0
vvthangdev <vvthang.dev@gmail.com>
2024-07-05 10:42:51 +0700

    update

---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private int year;
+    private String name;
+
+    public Student(String name, int year) {
+        this.year = year;
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
