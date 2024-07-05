04ead89d2e79293e1de96127d52ca72e034cfab5
vvthangdev <vvthang.dev@gmail.com>
2024-07-05 16:08:23 +0700

    Bai 6

--- src/Bai6/Main.java
+++ src/Bai6/Main.java
@@ -5,38 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(true){
-            System.out.println("Nhap exit de thoat");
-            System.out.println("Nhap thong tin NV: ");
-            String tenNhanVien = sc.nextLine();
-            if(tenNhanVien.equalsIgnoreCase("exit")) {
-                break;
-            }
-            System.out.println("Nhap luong co ban: ");
-            double luongCoBan = sc.nextDouble();
-            System.out.println("Nhap he so luong: ");
-            double heSoLuong = sc.nextDouble();
-            sc.nextLine();
-            NhanVien nv = new NhanVien(tenNhanVien, luongCoBan, heSoLuong);
-            nv.inTTin();
-
-            while (true) {
-                System.out.println("Tang he so luong them x =  ");
-                double heSo = sc.nextDouble();
-                sc.nextLine();
-                if (heSo == 0) break;
-                if (nv.tangLuong(heSo)) {
-                    System.out.println("Tang luong thanh cong");
-                } else {
-                    System.out.println("Tang luong that bai");
-                }
-                nv.inTTin();
-            }
-            System.out.println("So luong nhan vien da duoc tao: " + NhanVien.getSoLuongNhanVien());
-        }
+        System.out.println("Nhap ten phong ban: ");
+        String tenPhongBan = sc.nextLine();
+        System.out.println("Nhap so nhan vien: ");
+        byte soNhanVien = sc.nextByte();
+        sc.nextLine();
+        PhongBan pb = new PhongBan(tenPhongBan, soNhanVien);
+        NhanVien nv1 = new NhanVien("Vu Van Thang", 2);
+        NhanVien nv2 = new NhanVien("Cao Thi Le Quyen", 2);
+        pb.setTenPhongBan(tenPhongBan);
+        pb.themNV(nv1);
+        pb.themNV(nv2);
+        pb.inTTin();
+        pb.xoaNV();
+        pb.inTTin();
         sc.close();
-        // In ra số lượng nhân viên đã được tạo
-        System.out.println("Tong so nhan vien da duoc tao: " + NhanVien.getSoLuongNhanVien());
-        System.out.println("Tong so tien luong cua nhan vien la: " + NhanVien.getTongLuong());
     }
 }
--- src/Bai6/NhanVien.java
+++ src/Bai6/NhanVien.java
@@ -2,81 +2,53 @@ package Bai6;
 
 public class NhanVien {
     private String tenNhanVien;
-    private double luongCoBan;
     private double heSoLuong;
-    public static final double LUONG_MAX = 3000;
-    private static int cnt = 0;
-    private static double tongLuong = 0;
+    public static final double LUONG_MAX = 20000000;
+    public static final double LUONG_CO_BAN = 750000;
 
-    //getter:
-
-    public NhanVien(String tenNhanVien, double luongCoBan, double heSoLuong) {
+    public NhanVien(String tenNhanVien, double heSoLuong) {
         this.tenNhanVien = tenNhanVien;
-        this.luongCoBan = luongCoBan;
         this.heSoLuong = heSoLuong;
-        cnt++;// Tang so luong nhan vien them 1
-        tongLuong += tinhLuong();
     }
 
-    public static int getSoLuongNhanVien(){
-        return cnt;
-    }
     //Getter va Setter
-
-    public String getTenNhanVien(){
+    public String getTenNhanVien() {
         return tenNhanVien;
     }
-    public void setTenNhanVien(String tenNhanVien){
+
+    public void setTenNhanVien(String tenNhanVien) {
         this.tenNhanVien = tenNhanVien;
-    }
-
-    public double getLuongCoBan() {
-        return luongCoBan;
-    }
-
-    public void setLuongCoBan(double luongCoBan) {
-        this.luongCoBan = luongCoBan;
     }
 
     public double getHeSoLuong() {
         return heSoLuong;
     }
 
-    public void setHeSoLuong(double heSoLuong){
-        tongLuong -= tinhLuong();
+    public void setHeSoLuong(double heSoLuong) {
         this.heSoLuong = heSoLuong;
-        tongLuong += tinhLuong();
     }
 
-    //===================
     //Cac phuong thuc:
     public double tinhLuong(){
-        return this.luongCoBan * this.heSoLuong;
+        return this.LUONG_CO_BAN * this.heSoLuong;
     }
 
     public void inTTin(){
         System.out.println("Ten nhan vien: " + tenNhanVien);
-        System.out.println("Luong co ban: " + luongCoBan);
         System.out.println("He so luong: " + heSoLuong);
         System.out.println("Luong: " + tinhLuong());
     }
 
     public boolean tangLuong(double heSo){
-        double luongMoi = luongCoBan * (heSoLuong + heSo);
+        double luongMoi = LUONG_CO_BAN * (heSoLuong + heSo);
         if (luongMoi > LUONG_MAX) {
             System.out.println("Khong the tang!");
             return false;
         }
         else {
-            tongLuong -= tinhLuong();
             heSoLuong = heSoLuong + heSo;
-            tongLuong += tinhLuong();
             System.out.println("Duoc phep tang luong");
             return true;
         }
-    }
-
-    public static double getTongLuong(){
-        return tongLuong;
     }
 }
